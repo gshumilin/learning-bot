12cc2b4d9e36f573e4453e115a67269fdd25a075
gshumilin <gshumilin1234@gmail.com>
2022-05-16 17:47:42 +0700

    Removed redundant commented sections

--- src/Realisations/Config.hs
+++ src/Realisations/Config.hs
@@ -9,8 +9,6 @@ import           Data.Aeson.Types
 import           Control.Applicative
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Text as T
---import qualified Data.Text.Lazy.IO as T
---import qualified Data.Text.Lazy.Encoding as T
 import           Data.Foldable (asum)
 import App.TelegramFromJSON
 import App.TelegramToJSON
